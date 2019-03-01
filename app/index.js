589dafcbd9e9feae2ac1b4ac5b6389f04a013aee
Wenzel Massag <mail@wmassag.de>
2019-03-01 13:52:21 +0100

    Minimal React in index.js

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+const app = document.getElementById('app');
+
+// In diesem Projekt wurde schon der React Router genutzt. Der Wechsel der Views ist in pages/Layout.js abgebildet.  
+ReactDOM.render(
+  <div>
+    React App here.
+  </div>,
+  app
+);
