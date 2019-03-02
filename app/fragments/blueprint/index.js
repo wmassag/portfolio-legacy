b23063cd835598e32f25164094f084596311f313
Wenzel Massag <mail@wmassag.de>
2019-03-02 15:45:53 +0100

    Moved Layout into app/fragments/pages/Layout.js Created Component blueprint app/fragments/blueprint

---
+++ app/fragments/blueprint/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+require("./style.scss")
+
+export default class Layout extends React.Component {
+  constructor(props){
+    super(props)
+    
+    //more to come
+  }
+  
+  render(){
+    return(
+      <div id="__blueprint">
+        Copmonent Name should be wrapper ID and needs to be updated in style.scss
+      </div>
+    )
+  }
+}
