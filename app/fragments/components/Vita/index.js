4d6aa4244ab1f01f8d2d885f8f4514a1d48be4b8
Wenzel Massag <mail@wmassag.de>
2019-05-09 17:24:37 +0200

    Vita component with sample content and new, globally applied styles

---
+++ app/fragments/components/Vita/index.js
@@ -0,0 +1,33 @@
+/*
+  Component needs to render HTML or Markup, options:
+  - https://www.npmjs.com/package/html-react-parser
+  - https://www.npmjs.com/package/markdown
+*/
+
+import React from "react"
+
+require("./style.scss")
+
+export default class Vita extends React.Component {
+  constructor(props){
+    super(props)
+    
+    //more to come
+  }
+  
+  render(){
+    function dangerousHTML(){
+      return {
+        __html: "<p>Currently available for hire as an intern (UX, UI, code), I'm a student of <a href='http://digitaldesign.io/' target='_blank'>digitaldesign.io</a> at the University of Applied Sciences Ravensburg-Weingarten (HRW / <a href='http://hs-weingarten.de' target='_blank'>hs-weingarten.de</a>). I'm ready to see the world and move to a new city, country or continent, who knows, maybe one day even to another planet!</p><p>My curiosity in all things design, technology and science has been the driving force behind my decisions ever since I was a child. It has lead me to become a coding, digital designer with a focus on innovation.</p><p>I'm happy to hear from you: <a href='mailto:mail@wmassag.de'>mail@wmassag.de</a>.</p>"
+      }
+    }
+            
+    return(
+      <div className="__Vita">
+        <h2 className="title">About Me</h2>
+        <img src="/static/vita/portrait-2019.jpg" alt="Portrait photo of Wenzel Massag. Year: 2019." />
+        <section className="content" dangerouslySetInnerHTML={dangerousHTML()} />
+      </div>
+    )
+  }
+}
