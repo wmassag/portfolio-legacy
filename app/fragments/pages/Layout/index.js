252f2509320ddea0727d3c40a891e38550ebefbc
Wenzel Massag <mail@wmassag.de>
2019-05-17 10:42:33 +0200

    Content for homepage.

--- app/fragments/pages/Layout/index.js
+++ app/fragments/pages/Layout/index.js
@@ -25,9 +25,27 @@ export default class Layout extends React.Component {
           imageAlt="A rendered mockup of Proximity on iPhone"
           imageType="iPhoneMockup"
           
-          description="A location based social network, designed to connect people and business locally."
+          description="A location based social network, designed to connect people locally. This social network connects meaningful interpersonal relationships with local business."
           
           metaList={["UX Concept", "iOS UI", "Sketch, Adobe After Effects"]}
+        />
+        
+        <Card
+          title="CI + IT"
+          
+          imageURL="static/sapob/render.png"
+          imageAlt="A rendered mockup of the redesigned homepage for See-Apotheke on iPhone"
+          imageType="iPhoneMockup"
+          
+          description="Logo re-design, homepage, marketing materials, in-store displays and more for seeapotheke.de."
+          
+          metaList={
+            ["CI & UX",
+            "Web UI and Code",
+            "Digital and print ads",
+            "Website content",
+            "Server configuration"]
+          }
         />
         <Vita />
       </div>
