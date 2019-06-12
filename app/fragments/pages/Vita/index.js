0138485eafa918ee64c6d67756850e4211a8ef27
Wenzel Massag <mail@wmassag.de>
2019-06-12 22:43:24 +0200

    Vita component image is now background image so it can make use of background-size: cover

--- app/fragments/pages/Vita/index.js
+++ app/fragments/pages/Vita/index.js
@@ -21,17 +21,20 @@ export default class Vita extends React.Component {
         __html: "<p>Currently available for hire as an intern (UX, UI, code), I'm a student of <a href='http://digitaldesign.io/' target='_blank'>digitaldesign.io</a> at the University of Applied Sciences Ravensburg-Weingarten (HRW / <a href='http://hs-weingarten.de' target='_blank'>hs-weingarten.de</a>). I'm ready to see the world and move to a new city, country or continent, who knows, maybe one day even to another planet!</p><p>My curiosity in all things design, technology and science has been the driving force behind my decisions ever since I was a child. It has lead me to become a coding, digital designer with a focus on innovation.</p><p>I'm happy to hear from you: <a href='mailto:mail@wmassag.de'>mail@wmassag.de</a>.</p>"
       }
     }
+    
+    const portraitImage = {
+      backgroundImage: "url('static/vita/portrait-2019.jpg')"
+    }
             
     return(
-      <div className="__Vita">
+      <div id="__Vita">
         <div className="angledTransition">
           <div className="left"></div>
           <div className="right"></div>
         </div>
         <section className="limiter">
           <h2 className="title">About Me</h2>
-          <aside className="imageContainer">
-            <img src="static/vita/portrait-2019.jpg" alt="Portrait photo of Wenzel Massag. Year: 2019." />
+          <aside className="imageContainer" aria-label="Portrait photo of Wenzel Massag. Year: 2019." style={portraitImage}>
             <div className="overlayShadow"></div>
           </aside>
           <article className="content" dangerouslySetInnerHTML={dangerousHTML()} />
